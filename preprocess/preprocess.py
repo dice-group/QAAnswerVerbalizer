52c0388dfe7c39590042fc2f1b2df2659d10b7ea
parthsharma2695 <parthsharma2695@gmail.com>
2023-09-27 01:36:53 +0200

    Preprocess Vquanda and Quald9 datasets

---
+++ preprocess/preprocess.py
@@ -0,0 +1,198 @@
+"""DATASETS: QUALD-9 PLUS, VQUANDA
+"""
+
+import json
+import sys
+sys.path.append("./")
+from question_query_answer import question_query_answer
+from linking import linking
+import re
+from tqdm import tqdm
+import args 
+import constants as constants
+
+root_path= constants.get_project_root()
+ANS_TOKEN= '[ANS]'
+
+SYMBOLS= {'?uri': 'var_uri', '{': 'brack_open ',
+    '}': ' brack_close', '.': ' sep_dot ', '!=': 'not_equal', 
+    '=': 'equal'
+    }
+    
+def vquanda(path, num= 3999, verbose= True, mask_ans= True):
+    """
+    preprocess the the Vquanda dataset. 
+
+    Parameter
+    ---------
+
+    path: str
+    path of the json file of the dataset.
+
+    num: int
+    number of entries to be processed.
+
+    verbose: bool
+    if the query has to be made more natural language readable
+
+    mask_ans: bool
+    if the answers in the verbalizations have to be masked.
+
+
+    Returns: dict
+    -------------
+
+    dict of pre-processed index, question, query , verbalization
+    """
+    with open(path, 'r') as f:
+        data=json.loads(f.read())
+    final_dict= {}
+    for i in tqdm(range(num)):
+        d= data[i]
+        index= d['uid']
+        question = d['question']
+        verbalization = d['verbalized_answer']
+        query= d['query']
+
+        if query and question and verbalization is not None: 
+            if verbose:   
+                query= make_verbose_vquanda(query)
+            query=query.lower()
+        if mask_ans:
+            verbalization= mask_answer(verbalization)
+
+        info= {'index': index, 'question': question, 'query': query, 'verbalization': verbalization}
+        final_dict[i]= info
+    return final_dict
+
+        
+
+def quald(path, num, lang='en', verbose= True, mask_ans= True):
+    """
+    pre-process the quald-9 plus dataset.
+
+    Parameter
+    ---------
+
+    path: str
+    path of the json file of the dataset.
+
+    num: int
+    number of entries to be processed.
+
+    lang: str
+    language of the data: 'en' for English, 'de' for German
+
+    verbose: bool
+    if the query has to be made more natural language readable
+
+    mask_ans: bool
+    if the answers in the verbalizations have to be masked.
+
+
+    Returns: dict
+    -------------
+
+    dict of pre-processed index, question, query , verbalization
+
+    """
+
+
+    data=json.load(open(path))
+    final_dict= {}
+    for i in tqdm(range(num)):
+        d= data[str(i)]
+        qa= question_query_answer(d)
+        index=qa.get_id()
+        question= qa.get_question(lang)
+        query= qa.get_query()
+        verbalization= qa.get_verbalized()
+
+        if query and question and verbalization is not None:        
+            if verbose:
+                query= make_verbose(query, lang)
+            if mask_ans:
+                verbalization= mask_answer(verbalization)
+            query= query.lower()
+
+
+            info= {'index': index, 'question': question, 'query': query, 'verbalization': verbalization}
+            final_dict[i]= info
+    return final_dict
+
+def mask_answer(verbalization):
+    ans_pattern= r'\[.*?\]'
+    ans= re.findall(ans_pattern,verbalization)
+    verbalization= verbalization.replace(ans[0], ANS_TOKEN)
+    
+    return verbalization
+
+
+def make_verbose(query, lang):
+    """
+    Makes QUALD (based on wikidata) query into a more readable format, by replacing URIs with the respective labels and 
+    replacing Symbols and other operators with text descriptions.
+    """
+    # All Regular Expression Patterns to get entities, relations and prefix
+    ent_pattern= re.compile(r'wd:(Q\d+)|<http://www\.wikidata\.org/entity/(Q\d+)>')
+    rel_pattern= re.compile(r'(wdt|p|pq):(P\d+)\b|<http://www\.wikidata\.org/prop/direct/(P\d+)>')
+    prefix_pattern = re.compile(r'PREFIX [^:]+: <[^>]+>\s*')
+
+    ent = [e for sub in ent_pattern.findall(query) for e in sub if e]
+    rel= [r for sub in rel_pattern.findall(query) for r in sub if r]
+    ent =list(set(ent))
+    rel= list(set(rel))
+
+    for entity in ent:
+        label= linking(entity, lang)
+        query = query.replace("""wd:{e}""".format(e=entity), label)
+        query= query.replace("""<http://www.wikidata.org/entity/{e}>""".format(e= entity), label)
+    
+    for relation in rel:
+        label= linking(relation, lang)
+        query = query.replace("""wdt:{r}""".format(r=relation), label)
+        query=query.replace("""<http://www.wikidata.org/prop/direct/{r}>""".format(r=relation), label)
+    
+    for s in SYMBOLS:
+        if s in query:
+            query= query.replace(s, SYMBOLS[s])
+    query = prefix_pattern.sub('', query)
+
+    return query
+
+def make_verbose_vquanda(query):
+    """
+    Makes Vquanda(based on DBPedia) query into a more readable format, by replacing URIs with the respective labels and 
+    replacing Symbols and other operators with text descriptions.
+    """
+    pattern= re.compile(r'(<http://dbpedia.org/resource/[^>]+>) |(<http://dbpedia.org/property/[^>]+>) |(<http://dbpedia.org/ontology/[^>]+>)')
+    ent = [e for sub in pattern.findall(query) for e in sub if e]
+    ent =list(set(ent))
+    for e in ent:
+        label= linking(e, endpoint= 'dbpedia')
+        query=query.replace(e,label)
+    
+    type_url= '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>'
+    if type_url in query:
+        query= query.replace(type_url, 'type')
+    for s in SYMBOLS:
+        if s in query:
+            query= query.replace(s, SYMBOLS[s])
+    return query
+
+
+def write_to_file(dataset, data, name):
+    with open("""{path}/data/{dataset}/preprocessed_{dataset}_{name}.json""".format(path=root_path, dataset=dataset, name=name),'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+
+    filepath= """{path}/data/{dataset}/{name}.json""".format(path=root_path, dataset=constants.args.dataset, name= constants.args.name)
+    if constants.args.dataset == 'vquanda':
+        pre_data= vquanda(filepath, num= constants.args.num)
+        
+    if constants.args.dataset == 'quald':
+        pre_data= quald(filepath, num= constants.args.num, lang= constants.args.lang)
+    
+    write_to_file(constants.args.dataset, pre_data, constants.args.name)
