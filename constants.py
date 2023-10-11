6d85fb669c6af320fe89b43a2424965ce3eb2e51
parthsharma2695 <parthsharma2695@gmail.com>
2023-10-11 13:13:59 +0200

    Added Constants

--- constants.py
+++ constants.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import args as args
+import torch
+
 
 def get_project_root() -> Path:
     return Path(__file__).parent
@@ -7,3 +9,7 @@ def get_project_root() -> Path:
 
 parser = args.get_parser()
 args = parser.parse_args()
+
+TORCH_DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+ANS_TOKEN = '[ANS]'
+SEP_TOKEN = '<SEP>'
