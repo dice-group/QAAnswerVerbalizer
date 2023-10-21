0aad01d718a3d7d630da3dd5ddf23a72fc0add28
parthsharma2695 <parthsharma2695@gmail.com>
2023-10-21 16:27:49 +0200

    Changes to logger

--- test.py
+++ test.py
@@ -3,24 +3,41 @@ from utils import *
 import logging
 
 root_path = get_project_root()
-logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler(
-    """{path}/results/{dataset}_test.log""".format(path=root_path, dataset=args.dataset)), logging.StreamHandler()])
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+# logging.basicConfig(level=logging.INFO)
+
+con_handler = logging.StreamHandler()
+file_handler = logging.FileHandler("""{path}/results/{dataset}_{model}_test.log""".format(
+    path=root_path, dataset=args.dataset, model=args.model_name))
+con_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.INFO)
+con_handler.setFormatter(logging.Formatter(
+    "%(asctime)-7s %(name)s %(levelname)-7s %(message)s", "%Y-%m-%d %H:%M:%S"))
+file_handler.setFormatter(logging.Formatter(
+    "%(asctime)-7s %(name)s %(levelname)-7s %(message)s", "%Y-%m-%d %H:%M:%S"))
 logger = logging.getLogger(__name__)
+logger.addHandler(file_handler)
+logger.addHandler(con_handler)
 
 
 if __name__ == '__main__':
     filepath = """{path}/data/{dataset}/preprocessed_{dataset}_{name}.json""".format(
         path=root_path, dataset=args.dataset, name=args.name)
     data = make_df(path=str(filepath))
-    logging.info(
+    logger.info(
         """Length of Test Dataset: {length}""".format(length=len(data)))
 
     path = args.checkpoint_path
     torch_device = TORCH_DEVICE
-    logger.info("""--Loading Model: {model}--""".format(model=args.model_name))
+    logger.info(
+        """-- Loading {name} dataset {dataset}""".format(name=args.name, dataset=args.dataset))
+    logger.info("""-- Loading Model: {model} from {path} --""".format(
+        model=args.model_name, path=args.checkpoint_path))
     model = set_model(model_name=args.model_name,
                       path=path, device=torch_device)
-    logger.info("""--Loading Tokenizer: {model}--""".format(model=args.name))
+    logger.info(
+        """-- Loading Tokenizer: {model} --""".format(model=args.model_name))
     tokenizer = set_tokenizer(model_name=args.model_name, path=path)
 
     scorer = Score()
@@ -28,10 +45,9 @@ if __name__ == '__main__':
         data, model=model, tokenizer=tokenizer, torch_device=torch_device)
     scorer.save_to_file()
 
-    logging.info("""Meteor Score Average: {score}""".format(
+    logger.info("""Meteor Score Average: {score}""".format(
         score=scorer.meteor_avg.avg))
-    logging.info("""Bleu Score Average: {score}""".format(
+    logger.info("""Bleu Score Average: {score}""".format(
         score=scorer.bleu_avg.avg))
-    logging.info("""Sacre Bleu Score Average: {score}""".format(
+    logger.info("""Sacre Bleu Score Average: {score}""".format(
         score=scorer.sacrebleu_avg.avg))
-
