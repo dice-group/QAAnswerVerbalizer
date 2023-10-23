2aaa2eefa1d4a573edab626d555ea2a93476795c
parthsharma2695 <parthsharma2695@gmail.com>
2023-10-23 23:15:25 +0200

    Changed structure trainer into a class

--- training/train.py
+++ training/train.py
@@ -5,7 +5,48 @@ from constants import get_project_root
 from constants import args
 from constants import *
 from QAAVData import prepare_data, train_val_split
-import transformers
+from transformers import Trainer, TrainingArguments
+
+
+class FineTuningTrainer:
+    def __init__(self, root_path, model, train_data, eval_data, tokenizer, args):
+        self.root_path = root_path
+        self.model = model
+        self.train_data = train_data
+        self.eval_data = eval_data
+        self.tokenizer = tokenizer
+        self.args = args
+        self.training_args = self.get_training_args()
+        self.trainer = self.get_trainer()
+
+    def get_training_args(self):
+        output_directory = """{path}/output/{dataset}/{model}""".format(
+            path=root_path, dataset=args.dataset, model=args.model_name)
+        return TrainingArguments(
+            num_train_epochs=args.train_epochs,
+            output_dir=output_directory,
+            per_device_train_batch_size=1,
+            per_device_eval_batch_size=1,
+            save_steps=args.save_steps,
+            save_strategy='steps',
+            save_total_limit=4,
+            evaluation_strategy='steps',
+            eval_steps=args.eval_steps,
+            warmup_steps=100,
+            weight_decay=0.01,
+            load_best_model_at_end=True,
+        )
+
+    def get_trainer(self):
+        return Trainer(
+            model=self.model,
+            args=self.training_args,
+            train_dataset=self.train_data,
+            eval_dataset=self.eval_data,
+            tokenizer=self.tokenizer,)
+
+    def train(self):
+        self.trainer.train()
 
 
 if __name__ == '__main__':
@@ -22,31 +63,6 @@ if __name__ == '__main__':
         model_name=args.model_name, path=args.tokenizer_path)
 
     tr_data, val_data = prepare_data(tokenizer, train_data, val_data)
-
-    training_args = transformers.TrainingArguments(
-        output_dir="""{path}/output/{dataset}/{model}""".format(
-            path=root_path, dataset=args.dataset, model=args.model_name),
-        num_train_epochs=args.train_epochs,
-        per_device_train_batch_size=1,
-        per_device_eval_batch_size=1,
-        save_steps=args.save_steps,
-        save_strategy='steps',
-        save_total_limit=4,
-        evaluation_strategy='steps',
-        eval_steps=args.eval_steps,
-        warmup_steps=100,
-        weight_decay=0.01,
-        logging_dir="""{path}/output/{dataset}/{model}/logs""".format(
-            path=root_path, dataset=args.dataset, model=args.model_name),
-        logging_steps=10,
-        load_best_model_at_end=True,
-    )
-
-    trainer = transformers.Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=tr_data,
-        eval_dataset=val_data,
-        tokenizer=tokenizer,
-    )
-    trainer.train()
+    tr = FineTuningTrainer(root_path=root_path, model=model, train_data=tr_data,
+                           eval_data=val_data, tokenizer=tokenizer, args=args)
+    tr.train()
